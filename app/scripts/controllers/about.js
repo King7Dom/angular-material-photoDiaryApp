b0d181dcf9777deca49497ad581818ef53236455
Dominic Cheung <dom.with.a.inic@gmail.com>
2015-07-19 10:04:04 +1000

    Hello World!

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name photoDiaryApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the photoDiaryApp
+ */
+angular.module('photoDiaryApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name photoDiaryApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the photoDiaryApp
+ */
+angular.module('photoDiaryApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
